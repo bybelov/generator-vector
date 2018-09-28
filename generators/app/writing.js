675b794df0f8f4ea2715ef4c9f45b7aa0bd8b9b8
Igor <bybelov@yandex.ru>
2018-09-28 21:36:51 +0300

    fix folders name

--- generators/app/writing.js
+++ generators/app/writing.js
@@ -4,8 +4,7 @@ let _ = require('lodash');
 let path = require('path');
 let mkdirp = require('mkdirp');
 
-module.exports = function () {
-
+module.exports = function() {
   var props = this.props;
   var destPath = this.destinationPath();
 
@@ -15,7 +14,7 @@ module.exports = function () {
     capitalize: _.capitalize
   };
 
-  // dotfiles
+  // Dotfiles
   this.fs.copy(this.templatePath('gitignore'), '.gitignore');
   this.fs.copy(this.templatePath('eslintrc'), '.eslintrc');
   this.fs.copy(this.templatePath('babelrc'), '.babelrc');
@@ -23,42 +22,48 @@ module.exports = function () {
   this.fs.copy(this.templatePath('jsconfig.json'), 'jsconfig.json');
   this.fs.copy(this.templatePath('webpack.config.js'), 'webpack.config.js');
   this.fs.copy(this.templatePath('faviconData.json'), 'faviconData.json');
-  this.fs.copy(this.templatePath('README.md'),'README.md');
-  this.fs.copyTpl(this.templatePath('package.json'),'package.json', props);
+  this.fs.copy(this.templatePath('README.md'), 'README.md');
+  this.fs.copyTpl(this.templatePath('package.json'), 'package.json', props);
 
-  // gulp tasks
-  this.fs.copy(this.templatePath('gulp/**/*.*'),'gulp/');
+  // Gulp tasks
+  this.fs.copy(this.templatePath('gulp/**/*.*'), 'gulp/');
 
-  // src fonts, icons, images
+  // Src fonts, icons, images
   this.fs.copy(this.templatePath('src/fonts/**/*.*'), 'src/fonts/');
   this.fs.copy(this.templatePath('src/icons/*.svg'), 'src/icons/');
   this.fs.copy(this.templatePath('src/images/**/*.*'), 'src/images/');
 
-  // src pages
+  // Src pages
   this.fs.copy(this.templatePath('src/pages/**/*.*'), 'src/pages/');
 
-  // src js
+  // Src js
   this.fs.copy(this.templatePath('src/js/**/*.*'), 'src/js/');
   this.fs.copyTpl(this.templatePath('src/js/vendor.js'), 'src/js/vendor.js', props);
 
-  // src styles
+  // Src styles
   if (props.installBootstrap) {
-    this.fs.copy(this.templatePath('src/scss/0-settings/_variables-bootstrap.scss'), 'src/scss/0-settings/_variables.scss');
+    this.fs.copy(
+      this.templatePath('src/scss/0-settings/_variables-bootstrap.scss'),
+      'src/scss/0-settings/_variables.scss'
+    );
     mkdirp(path.join(destPath, 'src/scss/3-generic'));
-    mkdirp(path.join(destPath, 'src/scss/4-elements'));
+    mkdirp(path.join(destPath, 'src/scss/5-elements'));
     this.fs.copy(this.templatePath('src/scss/main-bootstrap.scss'), 'src/scss/main.scss');
-  }
-  else{
-    this.fs.copy(this.templatePath('src/scss/0-settings/_variables.scss'), 'src/scss/0-settings/_variables.scss');
+  } else {
+    this.fs.copy(
+      this.templatePath('src/scss/0-settings/_variables.scss'),
+      'src/scss/0-settings/_variables.scss'
+    );
     this.fs.copy(this.templatePath('src/scss/3-generic/**/*.*'), 'src/scss/3-generic/');
-    this.fs.copy(this.templatePath('src/scss/4-elements/**/*.*'), 'src/scss/4-elements/');
+    this.fs.copy(this.templatePath('src/scss/5-elements/**/*.*'), 'src/scss/5-elements/');
     this.fs.copy(this.templatePath('src/scss/main.scss'), 'src/scss/main.scss');
   }
   this.fs.copy(this.templatePath('src/scss/1-tools/**/*.*'), 'src/scss/1-tools/');
   this.fs.copy(this.templatePath('src/scss/2-fonts/**/*.*'), 'src/scss/2-fonts/');
-  mkdirp(path.join(destPath, 'src/scss/5-vendor'));
-  this.fs.copy(this.templatePath('src/scss/6-components/**/*.*'), 'src/scss/6-components/');
+  mkdirp(path.join(destPath, 'src/scss/4-vendor'));
+  this.fs.copy(
+    this.templatePath('src/scss/6-components/**/*.*'),
+    'src/scss/6-components/'
+  );
   this.fs.copy(this.templatePath('src/scss/7-helpers/**/*.*'), 'src/scss/7-helpers/');
-
-
 };
