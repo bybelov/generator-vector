0ab8dc467947e582a1f50ab780f9a24d668c927b
Igor <bybelov@yandex.ru>
2020-02-26 00:31:20 +0300

    Added sorting of the list of layout pages, update package imagemin, update packages

--- generators/app/templates/gulp/tasks/images.js
+++ generators/app/templates/gulp/tasks/images.js
@@ -4,31 +4,39 @@ import imagemin from 'gulp-imagemin';
 import newer from 'gulp-newer';
 import config from '../config';
 
-gulp.task('images', () => gulp
-
-  .src([
-    config.src.images + '/**/*.{jpg,png,jpeg,svg,gif}',
-    '!' + config.src.iconsPng + '/**/*.png'
-  ])
-  .pipe(newer(config.dest.images))
-  .pipe(plumber())
-  .pipe(imagemin([
-    imagemin.gifsicle({interlaced: true}), //сжатие .gif
-    imagemin.jpegtran({progressive: true}), //сжатие .jpg
-    imagemin.optipng({optimizationLevel: 3}), //степень сжатия от 0 до 7
-    imagemin.svgo({plugins: [{removeViewBox: false}]}) //сжатие .svg
-  ],{
-    verbose: true
-  }))
-  .pipe(gulp.dest(config.dest.images))
-
+gulp.task('images', () =>
+  gulp
+    .src([
+      config.src.images + '/**/*.{jpg,png,jpeg,svg,gif}',
+      '!' + config.src.iconsPng + '/**/*.png'
+    ])
+    .pipe(newer(config.dest.images))
+    .pipe(plumber())
+    .pipe(
+      imagemin(
+        [
+          imagemin.gifsicle({ interlaced: true }), // compress .gif
+          imagemin.mozjpeg({quality: 75, progressive: true}), // compress .jpg
+          imagemin.optipng({ optimizationLevel: 3 }), // optimization level, from 0 to 7
+          imagemin.svgo({ plugins: [{ removeViewBox: false }] }) // compress .svg
+        ],
+        {
+          verbose: true
+        }
+      )
+    )
+    .pipe(gulp.dest(config.dest.images))
 );
 
 const build = gulp => gulp.parallel('images');
-const watch = gulp => () => gulp.watch([
-                                          '!' + config.src.iconsPng + '/**/*.png', 
-                                          config.src.images + '/**/*.{jpg,png,jpeg,svg,gif}'
-                                      ], gulp.series('images'));
+const watch = gulp => () =>
+  gulp.watch(
+    [
+      '!' + config.src.iconsPng + '/**/*.png',
+      config.src.images + '/**/*.{jpg,png,jpeg,svg,gif}'
+    ],
+    gulp.series('images')
+  );
 
 module.exports.build = build;
 module.exports.watch = watch;
--- generators/app/templates/gulp/tasks/inject.js
+++ generators/app/templates/gulp/tasks/inject.js
@@ -3,39 +3,74 @@ import fs from 'fs';
 import inject from 'gulp-inject';
 import log from 'fancy-log';
 import colors from 'ansi-colors';
+import sort from 'sort-stream';
 import config from '../config';
 
-gulp.task('inject', () => gulp
+function getDataFromFile(file, replaceCategory) {
+  let fullPath = file.relative.replace(/\.[^/.]+$/, '');
+  fullPath = fullPath.replace(`/(${replaceCategory}\\)/`, '');
+  return JSON.parse(
+    fs.readFileSync(config.src.data + '/' + fullPath + '.json')
+  );
+}
 
-  .src(config.dest.pages + '/index.html')
-  .pipe(inject(
+gulp.task('inject', () =>
+  gulp
 
-    gulp.src(
-      [config.src.data + '/**/*.json'], {
-        read: false,
-        relative: true
-      }), {
-      transform: function(filePath, file) {
-        if (filePath.slice(-5) === '.json') {
-          const fullPath = file.relative.replace(/\.[^/.]+$/, '');
-          const data = JSON.parse(fs.readFileSync(config.src.data + '/' + fullPath + '.json'));
-
-          if (data.SETTINGS) {
-            if (data.SETTINGS.showOnIndexPage && data.SETTINGS.showOnIndexPage !== false) {
-              return '<li><a href="' + fullPath + '.html">' + data.SETTINGS.titleOnIndexPage + '</a></li>';
+    .src(config.dest.pages + '/index.html')
+    .pipe(
+      inject(
+        gulp
+          .src([config.src.data + '/**/*.json'], {
+            read: false,
+            relative: true
+          })
+          .pipe(
+            // sort by page name in JSON file
+            sort(function(a, b) {
+              let testA = getDataFromFile(a, 'data');
+              let testB = getDataFromFile(b, 'data');
+              let titleA = testA.SETTINGS.titleOnIndexPage;
+              let titleB = testB.SETTINGS.titleOnIndexPage;
+              if (titleA < titleB) {
+                return -1;
+              }
+              if (titleA > titleB) {
+                return 1;
+              }
+              return 0;
+            })
+          ),
+        {
+          transform: function(filePath, file) {
+            if (filePath.slice(-5) === '.json') {
+              const fullPath = file.relative.replace(/\.[^/.]+$/, '');
+              const data = getDataFromFile(file, 'page');
+              if (data.SETTINGS) {
+                if (
+                  data.SETTINGS.showOnIndexPage &&
+                  data.SETTINGS.showOnIndexPage !== false
+                ) {
+                  return (
+                    '<li><a href="' +
+                    fullPath +
+                    '.html">' +
+                    data.SETTINGS.titleOnIndexPage +
+                    '</a></li>'
+                  );
+                }
+              } else {
+                const errorMessage = `No data.SETTINGS object in file: ${file.path}`;
+                log.error(colors.yellow(errorMessage));
+              }
             }
-          } else {
-            const errorMessage = `No data.SETTINGS object in file: ${file.path}`;
-            log.error(colors.yellow(errorMessage));
+            // Use the default transform as fallback:
+            return inject.transform.apply(inject.transform, arguments);
           }
         }
-        // Use the default transform as fallback:
-        return inject.transform.apply(inject.transform, arguments);
-      }
-    }
-  ))
-  .pipe(gulp.dest(config.dest.pages))
-
+      )
+    )
+    .pipe(gulp.dest(config.dest.pages))
 );
 
 const build = gulp => gulp.series('inject');
